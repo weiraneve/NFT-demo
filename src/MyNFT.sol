8902da061d5177214fe51e0b9cd4a6754aad0f64
weiraneve <907221539@qq.com>
2024-09-24 20:31:55 +0800

    refactor: update constructor

--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -7,8 +7,8 @@ import "openzeppelin-contracts/contracts/access/Ownable.sol";
 contract MyNFT is ERC721, Ownable {
     uint256 public nextTokenId;
 
-    constructor() ERC721("MyNFT", "MNFT") Ownable() {
-        transferOwnership(msg.sender);
+    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {
+        
     }
 
     function mint(address to) external onlyOwner {
