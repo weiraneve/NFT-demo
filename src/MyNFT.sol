86bac9770256772fa412d824cb7137491ec52f87
weiraneve <907221539@qq.com>
2024-09-24 20:30:36 +0800

    refactor: update my nft constructor

--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721, Ownable {
     uint256 public nextTokenId;
 
-    constructor() ERC721("MyNFT", "MNFT") {}
+    constructor() ERC721("MyNFT", "MNFT") Ownable() {
+        transferOwnership(msg.sender);
+    }
 
     function mint(address to) external onlyOwner {
         _safeMint(to, nextTokenId);
